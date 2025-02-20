c9b7c2a6cde61b4d6dd18579f1c4661ec8660c0b
Nicolas <nicolasdelfino585@gmail.com>
2025-02-20 18:52:03 -0300

    trabajo casi terminado, dando estilos al boton

--- src/js/components/Todo.jsx
+++ src/js/components/Todo.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import "font-awesome/css/font-awesome.min.css";
 
 const Todo = () => {
   const [agregarItem, setAgregarItem] = useState([]);
@@ -11,33 +12,58 @@ const Todo = () => {
   }
 
   function borrarItem(index) {
-    let newArray = agregarItem.filter((_, idx) => index !== idx);
+    let newArray = agregarItem.filter((_, indx) => indx !== index);
     setAgregarItem(newArray);
+  }
+
+  function itemsLeft() {
+    let totalItems = agregarItem.length;
+    return (
+      <li
+        className="list-unstyled"
+        style={{ textAlign: "left" }}
+      >{`${totalItems} item${totalItems !== 1 ? "s" : ""} left`}</li>
+    );
   }
 
   const mapeo = agregarItem.map((valor, index) => {
     return (
-      <li key={valor} value={index}>
-        {valor}{" "}
-        <button className="boton" value={index} onClick={borrarItem}>
-          X
-        </button>
-      </li>
+      <div className="card">
+        <li
+          className="listaItems list-group-item"
+          key={index}
+          data-valor={valor}
+        >
+          {valor}{" "}
+          <button className="botonBorrar" onClick={() => borrarItem(index)}>
+            <i className="fa fa-times"></i>
+          </button>
+        </li>
+      </div>
     );
   });
 
   return (
-    <div className="container">
+    <div className="container w-50">
       <h1>todos</h1>
       <div>
-        <input
-          onKeyDown={handlerInput}
-          type="text"
-          placeholder="Agrega tu item"
-        />
-        <ul>{mapeo}</ul>
+        <div className="card w-100 mb-0">
+          <div className="card-body">
+            <input
+              className="input list-group-item w-100 h-290"
+              onKeyDown={handlerInput}
+              type="text"
+              placeholder="Agrega tu item"
+            />
+          </div>
+        </div>
+        <ul className="list-group">{mapeo}</ul>
+        <div className="card">
+          <div className="itemsLeft card-body">{itemsLeft()}</div>
+        </div>
       </div>
     </div>
   );
 };
+
 export default Todo;
